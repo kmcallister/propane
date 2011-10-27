4593ca6c65604b8f509e81fc90478b083c0a0a7d
Keegan McAllister <mcallister.keegan@gmail.com>
2011-10-27 03:29:39 -0400

    document

--- Propane/Transform.hs
+++ Propane/Transform.hs
@@ -1,38 +1,59 @@
+-- | Transformations on images, animations, etc.
 module Propane.Transform
-    ( scale, scale2, translate, rotate
+    ( -- * Transforming scalars
+      unbal, balun
+
+      -- * Manipulating space
+    , scale, scale2, translate, rotate
+
+      -- * Manipulating time
     , speed, shift
-    , unbal, balun
+
+      -- * Miscellaneous
     , spaced
     ) where
 
 import Propane.Types
 
+-- | Scale an @'Image'@ by the given factor.
 scale :: R -> Image a -> Image a
 scale s im (x,y) = im (x/s, y/s)
 
+-- | Scale an @'Image'@ by the given factors
+-- in /x/ and /y/ dimensions, respectively.
 scale2 :: (R,R) -> Image a -> Image a
 scale2 (sx,sy) im (x,y) = im (x/sx, y/sy)
 
+-- | Translate an @'Image'@ by some displacement.
 translate :: R2 -> Image a -> Image a
 translate (dx,dy) im (x,y) = im (x-dx, y-dy)
 
+-- | Rotate an @'Image'@ by some angle, in radians.
 rotate :: Angle -> Image a -> Image a
 rotate th im = \(x,y) -> im (x*cth + y*sth, y*cth + x*sth) where
     cth = cos (-th)
     sth = sin (-th)
 
+-- | Multiply @'Animation'@ speed by the given factor.
 speed :: Time -> Animation a -> Animation a
 speed r ani t = ani (r*t)
 
+-- | Shift an @'Animation'@ in time.  Positive offsets will
+-- cause things to happen later.
 shift :: Time -> Animation a -> Animation a
 shift dt ani t = ani (t-dt)
 
+-- | Squish the interval [-1, 1] into [0, 1].
 unbal :: R -> R
 unbal n = (n + 1) / 2
 
+-- | Stretch the interval [0, 1] into [-1, 1].
 balun :: R -> R
 balun n = (n * 2) - 1
 
+-- | @'spaced' n a b@ is a list of @n@ values, evenly spaced
+-- from @a@ (included) to @b@ (not included), such that the next
+-- evenly-spaced value would be @b@.
 spaced :: (Fractional a) => Count -> a -> a -> [a]
 spaced n a b = take n (iterate (+i) a) where
     i = (b - a) / fromIntegral n
--- Propane/Types.hs
+++ Propane/Types.hs
@@ -1,8 +1,20 @@
 {-# LANGUAGE
     DeriveDataTypeable #-}
+-- | Types for functional image synthesis.
 module Propane.Types
-    ( R, R2, Time, Angle, Count
+    ( -- * Basic type synonyms
+      --
+      -- | These exist in order to make type signatures
+      -- be better documentation.
+      R, R2, Time, Angle, Count
+
+      -- * Images and animations
     , Colour, Image, Animation
+
+      -- * Rasterization
+      --
+      -- | Images and animations can be converted to
+      -- discrete objects, e.g. for output to files.
     , Size(..), Raster(..), Rastimation(..), Word8
     ) where
 
@@ -13,24 +25,60 @@ import qualified Data.Sequence   as S
 import Data.Word
 import Data.Data ( Typeable, Data )
 
-type R     = Float
-type R2    = (R, R)
-type Time  = R
+-- | The (fake) real numbers.
+--
+-- This is the only place where we choose between @'Float'@ and @'Double'@.
+-- Switching to @'Double'@ would increase demands on memory bandwidth and
+-- cache space, decreasing performance (by 30% in one vaguely relevant
+-- test).
+type R = Float
+
+-- | Cartesian coordinates (/x/,/y/), representing points in the plane.
+type R2 = (R, R)
+
+-- | Time, in no particular unit.
+type Time = R
+
+-- | An angle, in radians.
 type Angle = R
+
+-- | A count, e.g. number of repetitions of something.
 type Count = Int
 
+-- | A colour with alpha channel.
+--
+-- @'C.AlphaColour'@ is defined in the @colour@ package, which also provides
+-- many functions for working with this type.
 type Colour = C.AlphaColour R
 
-type Image     a = R2 -> a
+-- | An image provides a value (such as colour) for every point of the
+-- real plane.
+type Image a = R2 -> a
+
+-- | An animation is a time-varying image.
 type Animation a = Time -> Image a
 
+-- | The number of pixels in a raster image.
 data Size = Size
     { sWidth  :: Int
     , sHeight :: Int }
     deriving (Eq, Ord, Read, Show, Typeable, Data)
 
+-- | A @'Raster'@ is a finite rectangle of an @'Image'@ with space and colour
+-- reduced to discrete quantities.
+--
+-- It is represented by a Repa @'R.Array'@ with indices of the form
+-- (@Z :. y :. x :. c@) where:
+--
+-- * (@x@, @y@) are pixel counts in the Cartesian /x/ and /y/ directions respectively.
+--
+-- * @c@ is a colour channel index: 0, 1, 2, 3 for red, green, blue, alpha respectively.
 newtype Raster = Raster (R.Array R.DIM3 Word8)
     deriving (Eq, Show, Typeable)
 
+-- | A @'Rastimation'@ is an animation of @'Raster'@ frames, where time has
+-- also been reduced to a discrete quantity.
+--
+-- It is represende as a sequence of @'Raster'@s.
 newtype Rastimation = Rastimation (S.Seq Raster)
     deriving (Eq, Show, Typeable)
